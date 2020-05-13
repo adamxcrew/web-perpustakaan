b677fb7e40f0dc5ff00ab1eeaa6744eb6a212b29
Andika Tedja <andikatedja@yahoo.com>
2020-05-13 22:58:10 +0800

    Fix landing page

--- index.php
+++ index.php
@@ -21,9 +21,9 @@
 
 <body id="page-top">
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-dark" id="mainNav">
         <div class="container">
-            <a class="navbar-brand js-scroll-trigger" href="#page-top">Perpustakaan Pintar Ilmu</a>
+            <a class="navbar-brand js-scroll-trigger" href="">Perpustakaan Pintar Ilmu</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -60,7 +60,7 @@
     <script src="js/bootstrap.bundle.min.js"></script>
 
     <!-- Custom scripts for this template -->
-    <script src="js/creative.min.js"></script>
+    <!-- <script src="js/creative.min.js"></script> -->
 </body>
 
 </html>
